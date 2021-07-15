3863a4fe9d4bd5a4424597f5dd017eed82813839
Pierre Gaimard <pierre@gaimard-family.fr>
2021-07-15 12:07:26 +0200

    Refactor EnvVarParserTest class

--- tests/Parser/EnvVarParserTest.php
+++ tests/Parser/EnvVarParserTest.php
@@ -3,6 +3,7 @@
 namespace Climbx\Config\Tests\Parser;
 
 use Climbx\Bag\Bag;
+use Climbx\Config\Exception\MissingEnvParameterException;
 use Climbx\Config\Parser\EnvVarParser;
 use PHPUnit\Framework\TestCase;
 
@@ -16,12 +17,6 @@ class EnvVarParserTest extends TestCase
         $bag = $this->createMock(Bag::class);
 
         $bag->expects($this->once())
-            ->method('has')
-            ->with(
-                $this->equalTo('FOO')
-            )->willReturn(true);
-
-        $bag->expects($this->once())
             ->method('get')
             ->with(
                 $this->equalTo('FOO')
@@ -29,26 +24,23 @@ class EnvVarParserTest extends TestCase
 
         $parser = new EnvVarParser($bag);
 
-        $parser->getParsedData('$env(FOO)');
+        $parser->getParsedData('myConfigFile', '$env(FOO)');
     }
 
     /**
      * @dataProvider stubMapProvider
      */
     public function testGetParsedData(
-        array $envStubHasMap,
         array $envStubGetMap,
         string $data,
         string $expectedResult
     ) {
-        // EnvVarParser dependency: env vars Bag.
         $envStub = $this->createStub(Bag::class);
-        $envStub->method('has')->willReturnMap($envStubHasMap); // Env Bag has($item) method
         $envStub->method('get')->willReturnMap($envStubGetMap); // Env Bag get($item) method
 
         $parser = new EnvVarParser($envStub);
 
-        $this->assertEquals($expectedResult, $parser->getParsedData($data));
+        $this->assertEquals($expectedResult, $parser->getParsedData('myConfigFile', $data));
     }
 
     /**
@@ -57,9 +49,23 @@ class EnvVarParserTest extends TestCase
     public function stubMapProvider(): array
     {
         return [
-            [[['FOO', true]], [['FOO', 'BAR']], '$env(FOO)', 'BAR'], // FOO key is found in Bag
-            [[['FOO', false]], [], '$env(FOO)', '$env(FOO)'], // FOO key is not found in Bag
-            [[['FOO', true]], [['FOO', 'BAR']], 'env(FOO)', 'env(FOO)'], // Expression do not match $env(VAR) pattern.
+            [[['FOO', 'BAR']], '$env(FOO)', 'BAR'], // FOO key is found in Bag
+            [[['FOO', 'BAR']], 'env(FOO)', 'env(FOO)'], // Expression do not match $env(VAR) pattern.
         ];
+    }
+
+    public function testGetParsedDataException()
+    {
+        $envStub = $this->createStub(Bag::class);
+        $envStub->method('get')->willReturn(false);
+
+        $parser = new EnvVarParser($envStub);
+
+        $this->expectException(MissingEnvParameterException::class);
+        $this->expectExceptionMessage(
+            'A reference to "FOO" .env parameter has been added to "myConfig" config file and is missing in .env file'
+        );
+
+        $parser->getParsedData('myConfig', '$env(FOO)');
     }
 }
