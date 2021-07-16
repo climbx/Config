442f205f51eda8824b23eac926956636c9363bd4
Pierre Gaimard <pierre@gaimard-family.fr>
2021-07-16 13:01:06 +0200

    Refactor loader interface

--- src/Loader/LoaderInterface.php
+++ src/Loader/LoaderInterface.php
@@ -2,18 +2,29 @@
 
 namespace Climbx\Config\Loader;
 
-use Climbx\Config\Exception\ConfigurationParserException;
+use Climbx\Config\Exception\ContainerExceptionInterface;
 
 interface LoaderInterface
 {
     /**
-     * Returns an array of a parsed configuration file raw data.
+     * Returns an array of a parsed configuration file data.
      *
-     * @param string $path
+     * If an error occurs while parsing file, a ContainerExceptionInterface exception is thrown.
+     *
+     * @param string $id
      *
      * @return array|false
      *
-     * @throws ConfigurationParserException
+     * @throws ContainerExceptionInterface
      */
-    public function load(string $path): array | false;
+    public function load(string $id): array | false;
+
+    /**
+     * Returns true if the config file is found and is readable, false otherwise.
+     *
+     * @param string $id
+     *
+     * @return bool
+     */
+    public function isReadable(string $id): bool;
 }
