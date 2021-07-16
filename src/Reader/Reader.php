018d48c47e22d0fa5b6de34bc0e19b444c1f4f30
Pierre Gaimard <pierre@gaimard-family.fr>
2021-07-16 13:04:39 +0200

    Add Reader class

---
+++ src/Reader/Reader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Climbx\Config\Reader;
+
+use Climbx\Config\Loader\LoaderInterface;
+use Climbx\Config\Parser\EnvVarParserInterface;
+
+class Reader implements ReaderInterface
+{
+    public function __construct(
+        private LoaderInterface $loader,
+        private EnvVarParserInterface $envVarParser,
+    ){
+    }
+
+    public function read(string $id): array | false
+    {
+        $rawData = $this->loader->load($id);
+
+        if ($rawData === false) {
+            return false;
+        }
+
+        return $this->envVarParser->getParsedData($id, $rawData);
+    }
+
+    public function isReadable(string $id): bool
+    {
+        return $this->loader->isReadable($id);
+    }
+}
